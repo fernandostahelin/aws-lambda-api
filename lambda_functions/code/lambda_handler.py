8ddb3aeb7aae6a90e9b5a4c26fb18eceb32f3452
Fernando Stahelin <fernandostahelin@gmail.com>
2024-08-21 21:02:14 -0300

    fixed datetime bug

--- lambda_functions/code/lambda_handler.py
+++ lambda_functions/code/lambda_handler.py
@@ -5,7 +5,7 @@ from writer import S3Writer
 
 def handler(event, context):
     coin = "BTC"
-    date = (datetime.datetime.now() - datetime.timedelta.days(2)).date()
+    date = (datetime.datetime.now() - datetime.timedelta(days=2)).date()
     api = MercadoBTCapi(coin=coin)
     data = api.get_data(date=date)
 
